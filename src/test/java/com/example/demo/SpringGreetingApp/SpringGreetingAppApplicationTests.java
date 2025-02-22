341dd2b3e627b14a286daf9c1ea09bc6857b85fa
Avinashkr000 <akum97543@gmail.com>
2025-02-22 20:35:14 +0530

    [Avinash kumar] UC1 Using GreetingController return JSON

---
+++ src/test/java/com/example/demo/SpringGreetingApp/SpringGreetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.SpringGreetingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGreetingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
